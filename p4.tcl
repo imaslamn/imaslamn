67599e9e9b921c3d1c5fdfdb216a4f79e11ab0b3
imaslamn <112794663+imaslamn@users.noreply.github.com>
2023-01-30 18:39:42 +0530

    Update p4.tcl

--- p4.tcl
+++ p4.tcl
@@ -1,34 +1,24 @@
-# Declare a new Simulator
+
 set ns [new Simulator]
 
-# Open the trace and nam file in write mode
+
 set tf [open out.tr w]
 set nf [open out.nam w]
 $ns trace-all $tf
 $ns namtrace-all $nf
 
-# Decide the topology: [tcp(0->2)], [udp(2->1)], [tcp(1->3)]
-#
-#      [tcp0]   [tcp1][null1]
-#       [n0]        [n1]
-#        |           |
-#        |           |
-#   ------------------------------ lan4
-#            |             |
-#            |             |
-#           [n3]          [n2]
-#         [sink3]     [udp2][sink2]
 
-# Create 4 nodes
+
+
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
 
-# Create lan and setup the link
+
 $ns make-lan -trace on "$n0 $n1 $n2 $n3" 10Mb 10ms LL Queue/DropTail Mac/802_3
 
-# Declare the required transport layer Protocols
+
 set tcp0 [new Agent/TCP]
 set tcp1 [new Agent/TCP]
 set udp2 [new Agent/UDP]
@@ -36,7 +26,7 @@ set null1 [new Agent/Null]
 set sink2 [new Agent/TCPSink]
 set sink3 [new Agent/TCPSink]
 
-# Attach these Protocols to their respective nodes
+
 $ns attach-agent $n0 $tcp0
 $ns attach-agent $n1 $tcp1
 $ns attach-agent $n2 $udp2
@@ -44,7 +34,7 @@ $ns attach-agent $n1 $null1
 $ns attach-agent $n2 $sink2
 $ns attach-agent $n3 $sink3
 
-# Declare Application layer protocols and attach them with their transport layer protocols
+
 set ftp0 [new Application/FTP]
 set ftp1 [new Application/FTP]
 set cbr2 [new Application/Traffic/CBR]
@@ -52,17 +42,17 @@ $ftp0 attach-agent $tcp0
 $ftp1 attach-agent $tcp1
 $cbr2 attach-agent $udp2
 
-# connect source to destination
+
 $ns connect $tcp0 $sink2
 $ns connect $udp2 $null1
 $ns connect $tcp1 $sink3
 
-# set the interval 
+ 
 $ftp0 set interval_ 0.001
 $ftp1 set interval_ 0.001
 $cbr2 set interval_ 0.01
 
-# define finish procedure
+
 proc finish {} {
 	global ns nf tf
 	$ns flush-trace
